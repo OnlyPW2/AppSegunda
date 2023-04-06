57e1c2fa28d56b99ed5a7a78792865db02e7f53e
Japinha Cheiroso <devwebpw@gmail.com>
2023-04-05 21:21:30 -0300

    primeiro commit

---
+++ AppSegunda/areas/Aluno/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppSegunda.areas.Aluno.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Aluno/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
